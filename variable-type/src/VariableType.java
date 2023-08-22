788dbd953a4255878e223e07e80bb462658c4ecb
gustavopnhro <gustavo.pnhro@gmail.com>
2023-08-22 13:37:30 -0300

    putting name in singular for follow a better practice

---
+++ variable-type/src/VariableType.java
@@ -0,0 +1,16 @@
+public class VariableType {
+    public static void main(String[] args) throws Exception {
+        //Primitive types
+
+        String name = "Gustavo";
+        short shortNumber = 1;
+        int IntegerNumber = 30000;
+        long longNumber = 999333666L;
+        double doubleNumber = 2.59392012231;
+
+        final float PI = 3.14F;
+        final int YEAR = 2023;
+
+
+    }
+}
