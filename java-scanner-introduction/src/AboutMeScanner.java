21a9b1955564bdc5511be03b0b8756b297afd4d8
gustavopnhro <gustavo.pnhro@gmail.com>
2023-08-23 08:33:26 -0300

    writing a example about scanner use

---
+++ java-scanner-introduction/src/AboutMeScanner.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+
+public class AboutMeScanner {
+    public static void main(String[] args) throws Exception {
+    Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+
+    System.out.println("Name ");
+    String name = scanner.next();
+
+    System.out.println("Lastname ");
+    String lastname = scanner.next();
+
+    System.out.println("Age ");
+    Integer age = scanner.nextInt();
+
+
+    System.out.println("My name is " + name + " " + lastname );
+    System.out.println("I'm " + age + " years old");
+
+    }
+}
