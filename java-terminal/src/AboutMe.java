d5e2768c88840af6c9de495a5124c04c873c2276
gustavopnhro <gustavo.pnhro@gmail.com>
2023-08-23 08:21:41 -0300

    writing examples to execute java files on terminal

---
+++ java-terminal/src/AboutMe.java
@@ -0,0 +1,12 @@
+public class AboutMe {
+
+    
+ 
+    public static void main(String[] args) {
+        String name = args [0];
+        String age = args [1];
+
+        System.out.println("Hi! My name is " + name);
+        System.out.println("I'm " + age + " years old");
+    }
+}
