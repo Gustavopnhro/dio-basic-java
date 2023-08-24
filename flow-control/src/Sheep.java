dee1b48412e3b4e51d2b42a0ca07c04ae459db73
gustavopnhro <gustavo.pnhro@gmail.com>
2023-08-24 05:53:44 -0300

    writing a for-loop example

---
+++ flow-control/src/Sheep.java
@@ -0,0 +1,9 @@
+public class Sheep {
+    public static void main(String[] args) {
+        for (int sheeps = 1; sheeps < 20; sheeps ++)
+        {
+            System.out.println("One more Sheep");
+        }
+        System.out.println("ZzZzZ");
+    }
+}
